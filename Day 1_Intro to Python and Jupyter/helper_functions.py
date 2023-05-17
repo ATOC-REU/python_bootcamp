890d28ddbee0332163389d89a0bc8775439ddecf
Sam Mogen <samuel.mogen@colorado.edu>
2023-05-17 13:18:25 -0600

    clear outputs + add function

---
+++ Day 1_Intro to Python and Jupyter/helper_functions.py
@@ -0,0 +1,13 @@
+# The following function will multiply an input value given by 5 and return that value
+def multiply_by_six(input_value):
+    return 6 * input_value
+
+# The following function defined as 'print_full_name' will print the combination of names and doesn't return any values
+def print_greeting(first_name, middle_initial, last_name):
+    print("Hello!")
+    print("My name is " + first_name + " " + middle_initial + " " + last_name)
+
+def print_graduation_age(student_age, years_to_graduation):
+    graduation_age = student_age + years_to_graduation
+    print("Graduation age is ", graduation_age)
+    
